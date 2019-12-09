0a759a202bc90c2f14aa521202dabf4f98d18160
Denis <denis@semi-neotech.com>
2019-12-09 20:01:48 +0900

    working files

--- research/object_detection/albatross/job_albatross_d1/job_env.sh
+++ research/object_detection/albatross/job_albatross_d1/job_env.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-. /home/denistestml/albatross/tool/albatross/set_env.sh
+. ./set_env.sh
 
-JOB_NAME=albatross_d1_v1_9
-OUTPUT_PATH=gs://$BUCKET_NAME/$JOB_NAME
+export JOB_NAME=albatross_d1_v1_19
+export OUTPUT_PATH=gs://$BUCKET_NAME/$JOB_NAME
 
 echo "JOB_NAME=$JOB_NAME"
 echo "OUTPUT_PATH=$OUTPUT_PATH"
