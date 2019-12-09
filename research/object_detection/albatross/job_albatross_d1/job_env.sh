f8da124c0db965a69edfa02f02c60c5adbb66e88
Denis <denis@semi-neotech.com>
2019-12-09 02:41:09 +0000

    update files

---
+++ research/object_detection/albatross/job_albatross_d1/job_env.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+. /home/denistestml/albatross/tool/albatross/set_env.sh
+
+JOB_NAME=albatross_d1_v1_9
+OUTPUT_PATH=gs://$BUCKET_NAME/$JOB_NAME
+
+echo "JOB_NAME=$JOB_NAME"
+echo "OUTPUT_PATH=$OUTPUT_PATH"
+
---
+++ research/object_detection/albatross/job_albatross_d1/training_cmd_5.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+cd ../../..
+
+gcloud ai-platform jobs submit training $JOB_NAME \
+	--packages dist/object_detection-0.1.tar.gz,slim/dist/slim-0.1.tar.gz,../../cocoapi/pycocotools-2.0.tar.gz \
+	--module-name object_detection.model_main \
+	--job-dir $OUTPUT_PATH \
+	--python-version 3.5 \
+	--runtime-version 1.14 \
+	--region $REGION \
+	--config object_detection/albatross/job_albatross_d1/cloud.yml \
+	-- \
+	--train_dir=$OUTPUT_PATH \
+	--pipeline_config_path=gs://${GCS_BUCKET}/data/ssd_mobilenet_v2_coco.config
+
